5b425606d8c79d28079da0718cf7935ad22ebf92
Irgicahya <iwan58767@gmail.com>
2025-04-16 14:39:54 +0700

    penambahan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,28 +2,26 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dashboard Admin</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Dashboard Admin</title>
 </head>
 
 <body>
-  <a class="nav-link" href="{{ route('siswa.index') }}">Data Siswa</a>
-  <a href="{{ route('logout') }}" onlick="event.preventDefault(); document.getElemenById('logout-form').submit();">Logout</a>
-  <form id="logout-form" action="{{ route('logout') }}" method="POST">
-    @csrf 
-</form>
-<h1>Dashboard Admin</h1>
-@if ($message = Session::get('success'))
-<p>{{ $message }}</p>
-@else
-<p>You are logged in!</p>
-@endif
+      <a class="nav-link" href="{{ route('siswa.index') }}">Data Siswa</a>
+      <a class="nav-link" href="{{ route('akun.index') }}">Data Akun</a>
+      <a class="nav-link" href="{{ route('pelanggaran.index') }}">Data Pelanggaran</a>
+      <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST">
+            @csrf
+      </form>
+      <h1>Dashboard Admin</h1>
+      @if ($message = Session::get('success'))
+      <p>{{ $message }}</p>
+      @else
+      <p>You are logged in!</p>
+      @endif
 
 </body>
-
-<footer>
-
-</footer>
 
 </html>
